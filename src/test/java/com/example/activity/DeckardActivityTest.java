f40dd5fcb80fa8ec16a43afe65c08189ca3d83cf
Lawrence Crowther and Lenny Turetsky <pair+lcrowther+lenny@pivotallabs.com>
2015-07-07 13:45:44 -0700

    add input field
    [#98582288]

--- src/test/java/com/example/activity/DeckardActivityTest.java
+++ src/test/java/com/example/activity/DeckardActivityTest.java
@@ -1,21 +1,41 @@
 package com.example.activity;
 
+import android.view.View;
+import android.widget.TextView;
+import com.example.R;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import com.example.BuildConfig;
+import org.robolectric.util.ActivityController;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
 public class DeckardActivityTest {
 
+    private ActivityController<DeckardActivity> activityController;
+    private DeckardActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activityController = Robolectric.buildActivity(DeckardActivity.class).create();
+        activity = activityController.get();
+    }
+
     @Test
-    public void testSomething() throws Exception {
-        assertTrue(Robolectric.buildActivity(DeckardActivity.class).create().get() != null);
+    public void activityCanBeCreated() throws Exception {
+        assertThat(activity).isNotNull();
+    }
+
+    @Test
+    public void inputTextMustBeSingleLine() throws Exception {
+        TextView input = (TextView) activity.findViewById(R.id.new_item_text);
+        assertThat(input.getMaxLines()).isEqualTo(1);
     }
 }
